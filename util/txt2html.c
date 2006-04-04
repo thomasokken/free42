b2f89acddc8393c252dc5ba6ea686011bfa0e753
thomas <thomasokken@gmail.com>
2006-04-04 13:48:03 +0000

    *** empty log message ***

--- util/txt2html.c
+++ util/txt2html.c
@@ -7,9 +7,9 @@ int main(int argc, char *argv[]) {
 	   "  <title>%s</title>\n"
 	   "<head>\n"
 	   "<body>\n"
-	   "  <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n"
+	   "  <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" height=\"100%\">\n"
 	   "    <tr><td valign=\"top\" align=\"center\" bgcolor=\"#ff60a0\"><a href=\"../index.html\"><img border=\"0\" src=\"../title.png\"></a></td>\n"
-	   "    <td><img src=\"../spacer.gif\" width=\"10\"></td><td>\n"
+	   "    <td><img src=\"../spacer.gif\" width=\"10\"></td><td valign=\"top\">\n"
 	   "<h3>%s</h3>\n"
 	   "<pre>", argv[1], argv[1]);
     while ((c = getchar()) != EOF) {
