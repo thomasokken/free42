3ac7db7a810507e4bff1542c6a9b5cc5fe9f9813
thomas <thomasokken@gmail.com>
2006-08-14 14:43:44 +0000

    *** empty log message ***

---
+++ windows/build-all.bat
@@ -0,0 +1,8 @@
+vcvars32
+copy-files
+msdev keymap2cpp.dsp /make "keymap2cpp - Win32 Release" /rebuild
+Release\keymap2cpp
+msdev skin2cpp.dsp /make "skin2cpp - Win32 Release" /rebuild
+Release\skin2cpp
+msdev Free42Binary.dsp /make "Free42Binary - Win32 Release" /rebuild
+msdev Free42Decimal.dsp /make "Free42Decimal - Win32 Release" /rebuild
