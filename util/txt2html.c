079c8459f55bc74af60a9a0f99b9ebc0df0815ca
thomas <thomasokken@gmail.com>
2007-12-06 04:17:21 +0000

    *** empty log message ***

--- util/txt2html.c
+++ util/txt2html.c
@@ -6,10 +6,7 @@ int main(int argc, char *argv[]) {
 	   "<head>\n"
 	   "  <title>%s</title>\n"
 	   "<head>\n"
-	   "<body>\n"
-	   "  <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" height=\"100%%\">\n"
-	   "    <tr><td valign=\"top\" align=\"center\" bgcolor=\"#ff60a0\"><a href=\"../index.html\"><img border=\"0\" src=\"../title.png\"></a></td>\n"
-	   "    <td><img src=\"../spacer.gif\" width=\"10\"></td><td valign=\"top\">\n"
+	   "<body style=\"background: no-repeat fixed left top; background-image: url(title.png); padding-left: 62px;\">\n"
 	   "<h3>%s</h3>\n"
 	   "<pre>", argv[1], argv[1]);
     while ((c = getchar()) != EOF) {
@@ -23,7 +20,6 @@ int main(int argc, char *argv[]) {
     printf("</pre>\n"
 	   "<p>\n"
 	   "Go <a href=\"index.html\">back</a>.\n"
-	   "</td></tr></table>\n"
 	   "</body>\n"
 	   "</html>\n");
     return 0;
