8821e4913bd221ed9d931c0d6f350dfc5629e91f
thomas <thomasokken@gmail.com>
2007-12-18 01:37:34 +0000

    *** empty log message ***

--- util/txt2html.c
+++ util/txt2html.c
@@ -20,6 +20,8 @@ int main(int argc, char *argv[]) {
     printf("</pre>\n"
 	   "<p>\n"
 	   "Go <a href=\"index.html\">back</a>.\n"
+	   "<p>\n"
+	   "<a href=\"http://sourceforge.net/\"><img src=\"http://sflogo.sourceforge.net/sflogo.php?group_id=211716&type=5\" width=\"210\" height=\"62\" border=\"0\" alt=\"SourceForge.net Logo\"/></a>\n"
 	   "</body>\n"
 	   "</html>\n");
     return 0;
