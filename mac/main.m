97fa09a958557c400c3989ee2fca0340de5f231f
thomas <thomasokken@gmail.com>
2009-06-19 13:49:40 +0000

    Getting started on Mac version (real application, non-Dashboard). Also, added transparency to Dashboard version's icon.

---
+++ mac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Free42
+//
+//  Created by Thomas Okken on 6/19/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
