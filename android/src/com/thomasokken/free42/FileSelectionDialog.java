23d347543fc6e7a1909f1dc9805e4831d6b48a95
thomas <thomasokken@gmail.com>
2011-01-15 20:35:50 +0000

    Some more file selection code.

--- android/src/com/thomasokken/free42/FileSelectionDialog.java
+++ android/src/com/thomasokken/free42/FileSelectionDialog.java
@@ -1,11 +1,15 @@
 package com.thomasokken.free42;
 
-import com.thomasokken.free42.PreferencesDialog.OkListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 import android.app.Dialog;
 import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -23,12 +27,14 @@ public class FileSelectionDialog extends Dialog {
 	private Button okButton;
 	private Button cancelButton;
 	private OkListener okListener;
+	private String currentPath;
 	
 	public FileSelectionDialog(Context ctx) {
 		super(ctx);
 		view = new FileSelView(ctx);
 		setContentView(view);
 		setTitle("Select File");
+		setPath("/");
 	}
 	
 	public interface OkListener {
@@ -37,6 +43,49 @@ public class FileSelectionDialog extends Dialog {
 
 	public void setOkListener(OkListener okListener) {
 		this.okListener = okListener;
+	}
+	
+	public void setPath(String path) {
+		String fileName = "";
+		File f = new File(path);
+		if (!f.exists() || f.isFile()) {
+			int p = path.lastIndexOf("/");
+			if (p == -1) {
+				fileName = path;
+				path = "/";
+			} else {
+				fileName = path.substring(p + 1);
+				path = path.substring(0, p);
+				if (path.length() == 0)
+					path = "/";
+			}
+		}
+		StringTokenizer tok = new StringTokenizer(path, "/");
+		List<String> pathComps = new ArrayList<String>();
+		pathComps.add("/");
+		StringBuffer pathBuf = new StringBuffer();
+		pathBuf.append("/");
+		while (tok.hasMoreTokens()) {
+			String t = tok.nextToken();
+			pathComps.add(t);
+			pathBuf.append(t);
+			pathBuf.append("/");
+		}
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, pathComps);
+		dirListSpinner.setAdapter(aa);
+		fileNameView.setText(fileName);
+		currentPath = pathBuf.toString();
+	}
+
+	private void doUp() {
+		if (currentPath.length() > 1) {
+			int n = currentPath.lastIndexOf("/", currentPath.length() - 2);
+			setPath(currentPath.substring(0, n + 1) + fileNameView.getText());
+		}
+	}
+	
+	private void doMkDir() {
+		// TODO
 	}
 	
 	private class FileSelView extends LinearLayout {
@@ -55,17 +104,39 @@ public class FileSelectionDialog extends Dialog {
 			ll2.setOrientation(VERTICAL);
 			upButton = new Button(ctx);
 			upButton.setText("Up");
+			upButton.setOnClickListener(new OnClickListener() {
+				public void onClick(View view) {
+					doUp();
+				}
+			});
 			lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 			lp.gravity = Gravity.CENTER_HORIZONTAL;
 			ll2.addView(upButton, lp);
 			mkdirButton = new Button(ctx);
 			mkdirButton.setText("MkDir");
+			mkdirButton.setOnClickListener(new OnClickListener() {
+				public void onClick(View view) {
+					doMkDir();
+				}
+			});
 			ll2.addView(mkdirButton, lp);
 			okButton = new Button(ctx);
 			okButton.setText("OK");
+			okButton.setOnClickListener(new OnClickListener() {
+				public void onClick(View view) {
+					if (okListener != null)
+						okListener.okPressed();
+					FileSelectionDialog.this.hide();
+				}
+			});
 			ll2.addView(okButton, lp);
 			cancelButton = new Button(ctx);
 			cancelButton.setText("Cancel");
+			cancelButton.setOnClickListener(new OnClickListener() {
+				public void onClick(View view) {
+					FileSelectionDialog.this.hide();
+				}
+			});
 			ll2.addView(cancelButton, lp);
 			lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 			lp.gravity = Gravity.CENTER_VERTICAL;
