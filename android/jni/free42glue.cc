fb730e918ee11f72fae52f4a1efd8f2f4c38cd07
thomas <thomasokken@gmail.com>
2011-04-20 19:11:07 +0000

    Removed obsolete core_audio_enabled() function from the JNI glue.

--- android/jni/free42glue.cc
+++ android/jni/free42glue.cc
@@ -84,8 +84,7 @@ static JNIEnv *getJniEnv() {
 
 /*******************************************************************/
 /* A couple of functions to enable the Java code to get the values */
-/* of the FREE42_MAGIC and FREE42_VERSION macros, and to test flag */
-/* 26 (audio enable, used to control keyboard click).              */
+/* of the FREE42_MAGIC and FREE42_VERSION macros.                  */
 /*******************************************************************/
 
 extern "C" jint
@@ -96,12 +95,6 @@ Java_com_thomasokken_free42_Free42Activity_FREE42_1MAGIC(JNIEnv *env, jobject th
 extern "C" jint
 Java_com_thomasokken_free42_Free42Activity_FREE42_1VERSION(JNIEnv *env, jobject thiz) {
     return FREE42_VERSION;
-}
-
-extern "C" jboolean
-Java_com_thomasokken_free42_Free42Activity_core_1is_1audio_1enabled(JNIEnv *env, jobject thiz) {
-    Tracer T("core_is_audio_enabled");
-    return flags.f.audio_enable;
 }
 
 
