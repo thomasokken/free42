8a0e9939e71b6f0d51fe64cff405db38160afa3d
thomas <thomasokken@gmail.com>
2013-01-06 00:12:49 +0000

    Mac version: renamed the *.m files to *.mm Made the links in the About window clickable.

---
+++ mac/Classes/URLTextField.mm
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * Free42 -- an HP-42S calculator simulator
+ * Copyright (C) 2004-2013  Thomas Okken
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/.
+ *****************************************************************************/
+
+#import "URLTextField.h"
+
+@implementation URLTextField
+
+- (void)encodeWithCoder:(NSCoder *)encoder{
+    [super encodeWithCoder:encoder];
+}
+
+- (id)initWithCoder:(NSCoder *)decoder {
+    [super initWithCoder:decoder];
+    return self;
+}
+
+- (void) mouseUp:(NSEvent *)theEvent {
+    NSString *urlStr = [self stringValue];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    [[NSWorkspace sharedWorkspace] openURL:url];
+}
+
+@end
