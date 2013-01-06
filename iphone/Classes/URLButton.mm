4593eb5483b997e64b3aa43540567a76403993cf
thomas <thomasokken@gmail.com>
2013-01-06 01:36:57 +0000

    iPhone version: made the links in the About window clickable. Renamed the *.m files to *.mm since they really are Objective-C++.

---
+++ iphone/Classes/URLButton.mm
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * Free42 -- an HP-42S calculator simulator
+ * Copyright (C) 2004-2013  Thomas Okken
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses/.
+ *****************************************************************************/
+
+#import "URLButton.h"
+
+@implementation URLButton
+
+- (void)encodeWithCoder:(NSCoder *)encoder{
+    [super encodeWithCoder:encoder];
+}
+
+- (id)initWithCoder:(NSCoder *)decoder {
+    [super initWithCoder:decoder];
+    [self addTarget:self action:@selector(buttonClicked) forControlEvents:UIControlEventTouchUpInside];
+    return self;
+}
+
+- (void) buttonClicked {
+    NSString *urlStr = self.titleLabel.text;
+    NSURL *url = [NSURL URLWithString:urlStr];
+    [[UIApplication sharedApplication] openURL:url];
+}
+
+@end
