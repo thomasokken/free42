59d79a09bcd06906ac349bf24073f965efc7b43b
thomas <thomasokken@gmail.com>
2014-03-01 20:28:03 +0000

    More Windows build tweaking.

--- windows/build-all.bat
+++ windows/build-all.bat
@@ -4,5 +4,5 @@ vcbuild keymap2cpp.vcproj "Release|Win32"
 Release\keymap2cpp
 vcbuild skin2cpp.vcproj "Release|Win32"
 Release\skin2cpp
-vcbuild "Free42 Binary.vcproj" "Release|Win32"
-vcbuild "Free42 Decimal.vcproj" "Release|Win32"
+vcbuild Free42Binary.vcproj "Release|Win32"
+vcbuild Free42Decimal.vcproj "Release|Win32"
