b1388fea46e08bde7024b1d793c5067f73ddd4aa
thomas <thomasokken@gmail.com>
2014-03-01 20:45:47 +0000

    Another Windows build fix.

--- windows/build-all.bat
+++ windows/build-all.bat
@@ -5,4 +5,8 @@ Release\keymap2cpp
 vcbuild skin2cpp.vcproj "Release|Win32"
 Release\skin2cpp
 vcbuild Free42Binary.vcproj "Release|Win32"
+move Release\Free42Binary.exe .
+rmdir /s /q Release
 vcbuild Free42Decimal.vcproj "Release|Win32"
+move Release\Free42Decimal.exe .
+rmdir /s /q Release
