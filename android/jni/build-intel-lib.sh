bf9a0fa7ef8c896ef3c796a530b7d5bc95f78b5a
thomas <thomasokken@gmail.com>
2017-03-25 22:41:32 +0000

    More Android FPTEST fixes.

--- android/jni/build-intel-lib.sh
+++ android/jni/build-intel-lib.sh
@@ -26,7 +26,7 @@ cd LIBRARY
 $MAKE CC=gcc CALL_BY_REF=1 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=0 _HOST_OS=Linux
 mv libbid.a ../../libgcc111libbid.a
 cd ../TESTS
-cp readtest.c ../..
+cp readtest.c readtest.h test_bid_conf.h test_bid_functions.h ../..
 cd ../..
 
 ( echo '#ifdef FREE42_FPTEST'; echo 'const char *readtest_lines[] = {'; tr -d '\r' < IntelRDFPMathLib20U1/TESTS/readtest.in | sed 's/^\(.*\)$/"\1",/'; echo '0 };'; echo '#endif' ) > readtest_lines.cc
