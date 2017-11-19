a07ca67783c4d6855f16c637c017210e7f14b43b
thomas <thomasokken@gmail.com>
2017-11-19 20:37:50 +0000

    Adding test logic for SEED(0); back out this change after testing all platforms!

--- common/core_commands2.cc
+++ common/core_commands2.cc
@@ -284,6 +284,8 @@ int docmd_seed(arg_struct *arg) {
             s = s * 10 + 1;
             random_number_high = s / 10000000LL;
             random_number_low = s % 100000000LL;
+            vartype *v = new_real(random_number_low % 1000LL);
+            recall_result(v);
             return ERR_NONE;
         }
         if (x < 0)
