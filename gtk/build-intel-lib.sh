ecb77826c4c16a070a1e560e9abfcbb8228ed98f
thomas <thomasokken@gmail.com>
2019-08-07 21:24:34 +0000

    Portability fixes for GTK build. Supports big-endian and FreeBSD out of the box now.

--- gtk/build-intel-lib.sh
+++ gtk/build-intel-lib.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 if [ -f gcc111libbid.a ]; then exit 0; fi
+
+if [ -z $MK ]; then
+  which gmake >/dev/null
+  if [ $? -eq 0 ]; then
+    MK=gmake
+  else
+    MK=make
+  fi
+fi
+
+which gcc >/dev/null
+if [ $? -eq 0 ]; then
+  CC=gcc
+else
+  CC=cc
+fi
+
+if [ `uname -s` == "FreeBSD" ]; then
+  OS_ARG="CFLAGS_OPT=-DLINUX"
+else
+  OS_ARG=
+fi
+
+if [ `echo ab | od -x | awk '{print $2}'` == "6162" ]; then
+  ENDIAN_ARG="-e BID_BIG_ENDIAN=1"
+else
+  ENDIAN_ARG=
+fi
+
 tar xvfz ../inteldecimal/IntelRDFPMathLib20U1.tar.gz
 cd IntelRDFPMathLib20U1
 # When building for architectures other than x86 or x86_64, I comment out the
@@ -17,7 +46,7 @@ cd LIBRARY
 # command line. The library will work even if you don't, but the state files
 # written by Free42 will only be compatible with those written on little-endian
 # platforms if you do.
-make CC=gcc CALL_BY_REF=1 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=0
+$MK $OS_ARG CC=$CC CALL_BY_REF=1 GLOBAL_RND=1 GLOBAL_FLAGS=1 UNCHANGED_BINARY_FLAGS=0 $ENDIAN_ARG
 mv libbid.a ../../gcc111libbid.a
 cd ../..
 ( echo '#ifdef FREE42_FPTEST'; echo 'const char *readtest_lines[] = {'; tr -d '\r' < IntelRDFPMathLib20U1/TESTS/readtest.in | sed 's/^\(.*\)$/"\1",/'; echo '0 };'; echo '#endif' ) > readtest_lines.cc
