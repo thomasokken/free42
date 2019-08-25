3daea22c3490378fed5d6dbec1c4ae7c3f5a320d
thomas <thomasokken@gmail.com>
2019-08-25 14:08:28 +0000

    Re-added FileImportActivity in Android version; completely unselective this time. Seems to work OK now.

---
+++ android/app/src/main/java/com/thomasokken/free42/FileImportActivity.java
@@ -0,0 +1,149 @@
+package com.thomasokken.free42;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+public class FileImportActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        System.err.println("FileImportActivity action = " + action);
+        Uri uri = intent.getData();
+        System.err.println("FileImportActivity uri = " + uri);
+
+        // If attachment, some contortions to try and get the original file name
+        String baseName = null;
+        String scheme = uri.getScheme();
+        if (scheme.equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, new String[]{MediaStore.MediaColumns.DISPLAY_NAME}, null, null, null);
+            cursor.moveToFirst();
+            int nameIndex = cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME);
+            if (nameIndex >= 0) {
+                baseName = cursor.getString(nameIndex);
+            }
+        } else {
+            baseName = uri.getLastPathSegment();
+        }
+        if (baseName == null || baseName.equals(""))
+            baseName = "Imported State";
+        else if (baseName.toLowerCase().endsWith(".f42"))
+            baseName = baseName.substring(0, baseName.length() - 4);
+
+        int n = 0;
+        String name;
+        String baseDir = getFilesDir().getAbsolutePath();
+        while (true) {
+            n++;
+            name = baseDir + "/" + baseName;
+            if (n > 1)
+                name += " " + n;
+            name += ".f42";
+            if (!new File(name).exists())
+                break;
+        }
+
+        // Network access not allowed on main thread, so...
+        if (scheme.equals("http") || scheme.equals("https")) {
+            new NetworkLoader(uri.toString(), name).start();
+            return;
+        }
+
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = getContentResolver().openInputStream(uri);
+            os = new FileOutputStream(name);
+            byte[] buf = new byte[8192];
+            // Check magic number first; don't bother importing if this is wrong
+            n = is.read(buf, 0, 4);
+            if (n != 4 || buf[0] != '2' || buf[1] != '4' || buf[2] != 'k' || buf[3] != 'F')
+                throw new FormatException();
+            os.write(buf, 0, 4);
+            while ((n = is.read(buf)) >= 0)
+                os.write(buf, 0, n);
+            wrapUp(null);
+        } catch (Exception e) {
+            wrapUp(e);
+        } finally {
+            if (is != null)
+                try {
+                    is.close();
+                } catch (IOException e) {}
+            if (os != null)
+                try {
+                    os.close();
+                } catch (IOException e) {}
+        }
+    }
+
+    private void wrapUp(Exception e) {
+        if (e == null) {
+            Free42Activity.showAlert("State imported.");
+        } else if (e instanceof FormatException) {
+            Free42Activity.showAlert("Invalid state format.");
+        } else {
+            e.printStackTrace();
+            Free42Activity.showAlert("State import failed.");
+        }
+        finish();
+    }
+
+    private class NetworkLoader extends Thread {
+        private String srcUrl, dstFile;
+        private Exception ex;
+        public NetworkLoader(String srcUrl, String dstFile) {
+            this.srcUrl = srcUrl;
+            this.dstFile = dstFile;
+        }
+        public void run() {
+            InputStream is = null;
+            OutputStream os = null;
+            try {
+                is = new URL(srcUrl).openStream();
+                os = new FileOutputStream(dstFile);
+                byte[] buf = new byte[8192];
+                // Check magic number first; don't bother importing if this is wrong
+                int n = is.read(buf, 0, 4);
+                if (n != 4 || buf[0] != '2' || buf[1] != '4' || buf[2] != 'k' || buf[3] != 'F')
+                    throw new FormatException();
+                os.write(buf, 0, 4);
+                while ((n = is.read(buf)) >= 0)
+                    os.write(buf, 0, n);
+            } catch (Exception e) {
+                ex = e;
+            } finally {
+                if (is != null)
+                    try {
+                        is.close();
+                    } catch (IOException e) {}
+                if (os != null)
+                    try {
+                        os.close();
+                    } catch (IOException e) {}
+            }
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    wrapUp(ex);
+                }
+            });
+        }
+    }
+
+    private static class FormatException extends Exception {
+        public FormatException() {}
+    }
+}
