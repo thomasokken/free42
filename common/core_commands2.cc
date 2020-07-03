0e1b28c5d6a81c33c00bb33af716d3752b8cedf1
Thomas Okken <thomasokken@gmail.com>
2020-07-03 12:44:46 -0400

    Fixed stack lift behavior with GETKEY.

--- common/core_commands2.cc
+++ common/core_commands2.cc
@@ -957,6 +957,7 @@ int docmd_varmenu(arg_struct *arg) {
 
 int docmd_getkey(arg_struct *arg) {
     mode_getkey = true;
+    mode_disable_stack_lift = flags.f.stack_lift_disable;
     return ERR_NONE;
 }
 
