fbc9be336cf3541a3bfd7536eb40eee28bef99cc
Thomas Okken <thomasokken@gmail.com>
2021-07-01 16:08:21 +0200

    L4STK fix
    The LNSTK fix in 3.0.2 was incorrect, and caused a crash in L4STK,
    among other issues.

--- common/core_globals.cc
+++ common/core_globals.cc
@@ -3261,7 +3261,7 @@ int push_stack_state(bool big) {
         free_vartype(st);
         goto nomem;
     }
-    if (!flags.f.big_stack) {
+    if (!big && flags.f.big_stack) {
         memcpy(st_data + 1, stack, save_levels * sizeof(vartype *));
         memmove(stack + n_dups, stack + sp - 3 + n_dups, (sp + 1 - save_levels) * sizeof(vartype *));
         memcpy(stack, dups, n_dups * sizeof(vartype *));
