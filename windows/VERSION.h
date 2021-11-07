67d3a64fe02773326eadd27943673a249b0a0d67
Thomas Okken <thomasokken@gmail.com>
2021-11-07 11:12:28 +0100

    Release 3.0.7

--- windows/VERSION.h
+++ windows/VERSION.h
@@ -1,3 +1,3 @@
-#define FREE42_VERSION_1 "Free42 3.0.6"
-#define FREE42_VERSION_2 "3.0.6\0"
-#define FREE42_VERSION_3 3,0,6,0
+#define FREE42_VERSION_1 "Free42 3.0.7"
+#define FREE42_VERSION_2 "3.0.7\0"
+#define FREE42_VERSION_3 3,0,7,0
