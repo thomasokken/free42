26a2593a262ea1eb3e93825c6d7238d31e57d80b
Thomas Okken <thomasokken@gmail.com>
2021-11-08 11:17:55 +0100

    Android native cleanup script, continued

--- android/app/src/main/cpp/clean.sh
+++ android/app/src/main/cpp/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 find . -type l -exec rm {} \;
-rm -f readtest.c readtest.h test_bid_conf.h test_bid_functions.h readtest_lines.cc
+rm -f readtest.c readtest.h readtest_lines.cc
+rm -f test_bid_conf.h test_bid_functions.h
 rm -f *.a
-rm -rf bin
+rm -rf bin IntelRDFPMathLib20U1
