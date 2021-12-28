efc52ab057fa1b6fcda9b330e56746d4a14341bb
Thomas Okken <thomasokken@gmail.com>
2021-12-28 18:21:27 +0100

    Fixed crash when launching without state

--- common/core_main.cc
+++ common/core_main.cc
@@ -111,7 +111,7 @@ void core_init(int read_saved_state, int4 version, const char *state_file_name, 
     }
     if (gfile != NULL)
         fclose(gfile);
-    if (state_file_name != NULL) {
+    if (read_saved_state != 0) {
         if (reason == 0) {
             my_rename(state_file_name_crash, state_file_name);
         } else {
