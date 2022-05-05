b3c66d4ee37191e8f81c1bcf9161d21e9586f091
Thomas Okken <thomasokken@gmail.com>
2022-05-05 11:02:15 +0200

    Release 3.0.11a

--- windows/VERSION.h
+++ windows/VERSION.h
@@ -1,3 +1,3 @@
-#define FREE42_VERSION_1 "Free42 3.0.11"
-#define FREE42_VERSION_2 "3.0.11\0"
-#define FREE42_VERSION_3 3,0,11,0
+#define FREE42_VERSION_1 "Free42 3.0.11a"
+#define FREE42_VERSION_2 "3.0.11a\0"
+#define FREE42_VERSION_3 3,0,11,1
