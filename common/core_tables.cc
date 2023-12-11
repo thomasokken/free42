9a52c15226d294b8ac21f7f21d6d7dbe57783ef1
Thomas Okken <thomasokken@gmail.com>
2023-12-11 22:19:51 +0100

    Removed unimplemented Time Module functions

--- common/core_tables.cc
+++ common/core_tables.cc
@@ -50,9 +50,8 @@
  */
 #define UNIM 0x00
 
-// Note about XROM numbers:
-// The Free42 extensions use
-// XROM 31,01-59 (a7c1-a7fb) (Data Logger ROM)
+// NOTE: There is a list of used and available XROM numbers
+// in the Plus42 version of this file.
 
 const command_spec cmd_array[] =
 {
@@ -381,39 +380,17 @@ const command_spec cmd_array[] =
 
     /* Time Module & CX Time support*/
     { /* ADATE */      docmd_adate,       "ADATE",               0x00, 0x00, 0xa6, 0x81,  5, ARG_NONE,   1, 0x01 },
-    { /* ALMCAT */     docmd_xrom,        "AL\315CAT",           0x04, 0x00, 0xa6, 0x82,  6, ARG_NONE,   0, UNIM },
-    { /* ALMNOW */     docmd_xrom,        "AL\315N\317W",        0x04, 0x00, 0xa6, 0x83,  6, ARG_NONE,   0, UNIM },
     { /* ATIME */      docmd_atime,       "ATIME",               0x00, 0x00, 0xa6, 0x84,  5, ARG_NONE,   1, 0x01 },
     { /* ATIME24 */    docmd_atime24,     "AT\311\315\30524",    0x00, 0x00, 0xa6, 0x85,  7, ARG_NONE,   1, 0x01 },
     { /* CLK12 */      docmd_clk12,       "CL\31312",            0x00, 0x00, 0xa6, 0x86,  5, ARG_NONE,   0, NA_T },
     { /* CLK24 */      docmd_clk24,       "CL\31324",            0x00, 0x00, 0xa6, 0x87,  5, ARG_NONE,   0, NA_T },
-    { /* CLKT */       docmd_xrom,        "CLKT",                0x04, 0x00, 0xa6, 0x88,  4, ARG_NONE,   0, UNIM },
-    { /* CLKTD */      docmd_xrom,        "CL\313TD",            0x04, 0x00, 0xa6, 0x89,  5, ARG_NONE,   0, UNIM },
-    { /* CLOCK */      docmd_xrom,        "CL\317\303K",         0x04, 0x00, 0xa6, 0x8a,  5, ARG_NONE,   0, UNIM },
-    { /* CORRECT */    docmd_xrom,        "CORR\305\303\324",    0x04, 0x00, 0xa6, 0x8b,  7, ARG_NONE,   0, UNIM },
     { /* DATE */       docmd_date,        "DATE",                0x00, 0x00, 0xa6, 0x8c,  4, ARG_NONE,   0, NA_T },
     { /* DATE_PLUS */  docmd_date_plus,   "DATE+",               0x00, 0x00, 0xa6, 0x8d,  5, ARG_NONE,   2, 0x01 },
     { /* DDAYS */      docmd_ddays,       "DDAYS",               0x00, 0x00, 0xa6, 0x8e,  5, ARG_NONE,   2, 0x01 },
     { /* DMY */        docmd_dmy,         "DMY",                 0x00, 0x00, 0xa6, 0x8f,  3, ARG_NONE,   0, NA_T },
     { /* DOW */        docmd_dow,         "DOW",                 0x00, 0x00, 0xa6, 0x90,  3, ARG_NONE,   1, 0x01 },
     { /* MDY */        docmd_mdy,         "MDY",                 0x00, 0x00, 0xa6, 0x91,  3, ARG_NONE,   0, NA_T },
-    { /* RCLAF */      docmd_xrom,        "RCLAF",               0x04, 0x00, 0xa6, 0x92,  5, ARG_NONE,   0, UNIM },
-    { /* RCLSW */      docmd_xrom,        "RC\314SW",            0x04, 0x00, 0xa6, 0x93,  5, ARG_NONE,   0, UNIM },
-    { /* RUNSW */      docmd_xrom,        "R\325NSW",            0x04, 0x00, 0xa6, 0x94,  5, ARG_NONE,   0, UNIM },
-    { /* SETAF */      docmd_xrom,        "SETAF",               0x04, 0x00, 0xa6, 0x95,  5, ARG_NONE,   0, UNIM },
-    { /* SETDATE */    docmd_xrom,        "S\305\324DATE",       0x04, 0x00, 0xa6, 0x96,  7, ARG_NONE,   0, UNIM },
-    { /* SETIME */     docmd_xrom,        "S\305TIME",           0x04, 0x00, 0xa6, 0x97,  6, ARG_NONE,   0, UNIM },
-    { /* SETSW */      docmd_xrom,        "SE\324SW",            0x04, 0x00, 0xa6, 0x98,  5, ARG_NONE,   0, UNIM },
-    { /* STOPSW */     docmd_xrom,        "ST\317\320SW",        0x04, 0x00, 0xa6, 0x99,  6, ARG_NONE,   0, UNIM },
-    { /* SW */         docmd_xrom,        "SW",                  0x04, 0x00, 0xa6, 0x9a,  2, ARG_NONE,   0, UNIM },
-    { /* T_PLUS_X */   docmd_xrom,        "T+X",                 0x04, 0x00, 0xa6, 0x9b,  3, ARG_NONE,   0, UNIM },
     { /* TIME */       docmd_time,        "TIME",                0x00, 0x00, 0xa6, 0x9c,  4, ARG_NONE,   0, NA_T },
-    { /* XYZALM */     docmd_xrom,        "XYZALM",              0x04, 0x00, 0xa6, 0x9d,  6, ARG_NONE,   0, UNIM },
-    { /* CLALMA */     docmd_xrom,        "CLAL\315A",           0x04, 0x00, 0xa6, 0x9f,  6, ARG_NONE,   0, UNIM },
-    { /* CLALMX */     docmd_xrom,        "CLAL\315X",           0x04, 0x00, 0xa6, 0xa0,  6, ARG_NONE,   0, UNIM },
-    { /* CLRALMS */    docmd_xrom,        "CLRALMS",             0x04, 0x00, 0xa6, 0xa1,  7, ARG_NONE,   0, UNIM },
-    { /* RCLALM */     docmd_xrom,        "RCLALM",              0x04, 0x00, 0xa6, 0xa2,  6, ARG_NONE,   0, UNIM },
-    { /* SWPT */       docmd_xrom,        "SWPT",                0x04, 0x00, 0xa6, 0xa3,  4, ARG_NONE,   0, UNIM },
 
     /* Intel Decimal Floating-Point Math Library: self-test */
     { /* FPTEST */     docmd_fptest,      "FPT\305ST",           0x00, 0x00, 0xa7, 0xd2,  6, ARG_NONE,   0, NA_T },
@@ -439,10 +416,6 @@ const command_spec cmd_array[] =
     { /* STOFLAG */    docmd_stoflag,     "STOFLAG",             0x00, 0x00, 0xa6, 0x6d,  7, ARG_NONE,  -1, 0x00 },
 
     /* User-defined functions */
-    // Note: a7db-a7dd encode FUNC0-FUNC2, superseded by FUNC 00, FUNC 11, and
-    // FUNC 21, and a7e0 encodes RTNERR without argument, and is superseded by
-    // RTNERR IND ST X... So we don't need those three XROMs any more, but we
-    // can't safely use them, so they should be left unassigned.
     { /* FUNC */       docmd_func,        "FUNC",                0x01, 0x00, 0xf2, 0xe0,  4, ARG_FUNC,   0, NA_T },
     { /* ERRMSG */     docmd_errmsg,      "ERRMSG",              0x00, 0x00, 0xa7, 0xf6,  6, ARG_NONE,   0, NA_T },
     { /* ERRNO */      docmd_errno,       "ERRNO",               0x00, 0x00, 0xa7, 0xf7,  5, ARG_NONE,   0, NA_T },
@@ -465,8 +438,6 @@ const command_spec cmd_array[] =
     { /* UNPICK */     docmd_unpick,      "UNPICK",              0x00, 0xf4, 0xf2, 0xa4,  6, ARG_NUM9,   0, NA_T },
     { /* RDNN */       docmd_rdnn,        "R\016N",              0x00, 0xf6, 0xf2, 0xa5,  3, ARG_NUM9,   0, NA_T },
     { /* RUPN */       docmd_rupn,        "R^N",                 0x00, 0xf7, 0xf2, 0xa6,  3, ARG_NUM9,   0, NA_T },
-    /* (Skipping 403 because of single-byte equality checks with CMD_END) */
-    { /* DUMMY */      NULL,              "",                    0x44, 0x00, 0x00, 0x00,  0, ARG_OTHER,  0, UNIM },
 
     /* Miscellaneous */
     { /* NOP */        docmd_nop,         "NOP",                 0x00, 0x00, 0x00, 0xf0,  3, ARG_NONE,   0, NA_T },
@@ -507,6 +478,8 @@ const command_spec cmd_array[] =
     { /* NN_TO_S */    docmd_nn_to_s,     "NN\017S",             0x00, 0x00, 0xa7, 0x1d,  4, ARG_NONE,   1, ALLT },
     { /* C_TO_N */     docmd_c_to_n,      "C\017N",              0x00, 0x00, 0xa7, 0xf1,  3, ARG_NONE,   1, 0x10 },
     { /* N_TO_C */     docmd_n_to_c,      "N\017C",              0x00, 0x00, 0xa7, 0xf2,  3, ARG_NONE,   1, 0x01 },
+    /* (Skipping 403 because of single-byte equality checks with CMD_END) */
+    { /* DUMMY */      NULL,              "",                    0x44, 0x00, 0x00, 0x00,  0, ARG_OTHER,  0, UNIM },
     { /* LIST_T */     docmd_list_t,      "LIST?",               0x00, 0x00, 0xa7, 0xf3,  5, ARG_NONE,   1, ALLT },
     { /* NEWLIST */    docmd_newlist,     "NEWLIST",             0x00, 0x00, 0xa7, 0xf4,  7, ARG_NONE,   0, NA_T },
     { /* TO_LIST */    docmd_to_list,     "\017LIST",            0x00, 0x00, 0xa6, 0xfc,  5, ARG_NONE,   1, 0x01 },
