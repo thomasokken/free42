cd4d7e09c0c3e7a1a30c6388b1c0bc1345a30d1a
Thomas Okken <thomasokken@gmail.com>
2024-03-07 15:45:25 +0100

    Fixed raw2txt/txt2raw build failure for Linux

--- common/raw2txt.cc
+++ common/raw2txt.cc
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string>
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- common/txt2raw.cc
+++ common/txt2raw.cc
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string>
 
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 
