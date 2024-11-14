509868ff344c76121f1da8a3c5d722546068321a
Thomas Okken <thomasokken@gmail.com>
2024-11-14 16:19:53 +0100

    Release 3.1.10

--- windows/VERSION.h
+++ windows/VERSION.h
@@ -1,3 +1,3 @@
-#define FREE42_VERSION_1 "Free42 3.1.9"
-#define FREE42_VERSION_2 "3.1.9\0"
-#define FREE42_VERSION_3 3,1,9,0
+#define FREE42_VERSION_1 "Free42 3.1.10"
+#define FREE42_VERSION_2 "3.1.10\0"
+#define FREE42_VERSION_3 3,1,10,0
