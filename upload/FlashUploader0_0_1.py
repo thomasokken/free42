98088f4d92525e9677d8f397f15f0d88e938ac21
Jeremy Leicht <94857347+Jeremy924@users.noreply.github.com>
2025-01-22 18:30:33 -0600

    Update FlashUploader0_0_1.py

--- upload/FlashUploader0_0_1.py
+++ upload/FlashUploader0_0_1.py
@@ -5,16 +5,19 @@ import serial.tools.list_ports
 
 # location on my pc: my_projects\stm-workspace\Free42\Debug\Free42.bin
 
-print("Flash loader 2.0\n")
+print("\033[32;1;5mFlash loader 2.0\033[0m\n")
 
 try:
-    file = open(input("Enter file to load to flash: "), 'rb')
+    file = open(input("Enter location of file to load to flash: "), 'rb')
 except FileNotFoundError:
     print("File not found")
     exit()
 
 data = file.read()
-print(len(data), "bytes to write")
+print('\n\033[4m' + str(len(data)) + "\033[0m bytes to write\n")
+
+print('To switch to program loader, disconnect power from calculator, then press and hold the top left button, and then connect power')
+
 
 print("\nAvailable Ports:")
 ports = serial.tools.list_ports.comports()
@@ -29,8 +32,8 @@ ser = serial.Serial(port, 9600, timeout=1)
 print("Erasing Chip")
 ser.write('echo off\nfrw\nwe\nec\n'.encode())
 
-for i in range(20):
-    print(f"Erasing chip: {i * 5}%")
+for i in range(30):
+    print(f"Erasing chip: {i * (100//30)}%")
     time.sleep(1)
     pass
 
@@ -46,6 +49,8 @@ for port in ports:
 
 ser = serial.Serial(input('Input new port:'), 9600, timeout=1)
 
+ser.write('echo off\nfrw\n'.encode())
+
 def print_status(current, total):
     print(f'Writing program: {current / total * 100:.1f}%')
 
@@ -53,11 +58,11 @@ size = 256
 sb = True
 count = 0
 for i in range(0, len(data), size):
-    if i < 299500:
+    if i < 200_000:
         continue
 
     if count == 20:
-        print_status(i - 299500, len(data) - 295000)
+        print_status(i - 200_000, len(data) - 200_000)
         count = 0
     d = data[i:i+size]
     if len(d) < size:
@@ -82,14 +87,14 @@ size = 256
 small_count = 0
 sb = True
 for i in range(0, len(data), size):
-    if i > 300000:
+    if i > 199_000:
         break
     d = data[i:i+size]
     if len(d) < size:
         d += b'\xFF' * (size - len(d))
     ser.write(f'W {i} {size} \n'.encode())
     if count == 20:
-        print_status(i, 300000)
+        print_status(i, 200_000)
         count = 0
     time.sleep(0.001)
     ser.write(d)
