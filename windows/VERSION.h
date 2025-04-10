e5590404862d6c09ab376851bfcfa2661ccdcde0
Thomas Okken <thomasokken@gmail.com>
2025-04-10 15:28:17 +0200

    Release 3.2.8

--- windows/VERSION.h
+++ windows/VERSION.h
@@ -1,3 +1,3 @@
-#define FREE42_VERSION_1 "Free42 3.2.7"
-#define FREE42_VERSION_2 "3.2.7\0"
-#define FREE42_VERSION_3 3,2,7,0
+#define FREE42_VERSION_1 "Free42 3.2.8"
+#define FREE42_VERSION_2 "3.2.8\0"
+#define FREE42_VERSION_3 3,2,8,0
